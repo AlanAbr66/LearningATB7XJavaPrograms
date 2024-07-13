523ac9fee831907fbd7ec7017d7f4815215067a0
Alan Abraham <alanabraham5671@gmail.com>
2024-07-13 13:20:59 +0530

    New files added and committed

---
+++ src/ex_10072024/lab014.java
@@ -0,0 +1,12 @@
+package ex_10072024;
+
+public class lab014 {
+    // Can I run class without a main method?
+    // run option?
+    static public void  main(String[] args) {
+
+    }
+    public static void main() {
+
+    }
+}
---
+++ src/ex_13072024/lab024.java
@@ -0,0 +1,8 @@
+package ex_13072024;
+
+public class lab024 {
+    public static void main(String[] args) {
+        String name = "Pramod";
+        System.out.printf("Your name is -> %s",name);
+    }
+}
