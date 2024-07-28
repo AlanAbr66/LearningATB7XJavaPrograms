99e196df7a0dffe3783082ccf0d1bd9eeea16945
Alan Abraham <alanabraham5671@gmail.com>
2024-07-28 09:37:44 +0530

    27 Jul 2024 Code Commited & Pushed

---
+++ src/ex_27072024/Salaries.java
@@ -0,0 +1,16 @@
+package ex_27072024;
+
+public class Salaries {
+    public static int main(String[] args) {
+        //  int [] salaries = {7, 10, 4, 13, 25, 18, 9, 8, 17};
+        //  int size = salaries.length;
+
+        //  Arrays.sort(salaries);
+
+        // int second = salaries[size-2];
+
+        // System.out.println("2nd largest salary is ::"+ second);
+
+        return 0;
+    }
+}
