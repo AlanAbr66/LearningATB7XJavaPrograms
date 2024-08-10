94a086889d87fd389ffd8973ecc7865665cc6856
Alan Abraham <alanabraham5671@gmail.com>
2024-08-10 12:34:28 +0530

    10th August 2024 (2) Code Commited & Pushed

---
+++ src/August/ex_10082024/SuperthisKeyword/lab183.java
@@ -0,0 +1,24 @@
+package August.ex_10082024.SuperthisKeyword;
+
+public class lab183 {
+
+}
+class Student extends Person {
+    // is A relation - inheritance
+    @Override
+    void message() {
+        System.out.println("I am Student message");
+    }
+
+    void display() {
+        super.message();
+    }
+
+
+}
+
+class Person {
+    void message() {
+        System.out.println("I am person message");
+    }
+}
