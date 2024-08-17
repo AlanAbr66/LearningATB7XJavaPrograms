a0187ff2f8d236dd58b2eabe4db4600f2b5e4356
Alan Abraham <alanabraham5671@gmail.com>
2024-08-17 12:08:23 +0530

    17th August 2024 Code Commited & Pushed

---
+++ src/August/ex_17082024/lab207.java
@@ -0,0 +1,9 @@
+package August.ex_17082024;
+
+import August.ex_17082024.COLORS;
+public class lab207 {
+    public static void main(String[] args) {
+        System.out.println("Color Code "+ COLORS.RED);
+        System.out.println("Color Code "+ COLORS.RED.getValue());
+    }
+}
