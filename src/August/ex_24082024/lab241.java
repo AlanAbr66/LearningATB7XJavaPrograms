768777c6f71f48c36bd30cfffc704a3773afb1a5
Alan Abraham <alanabraham5671@gmail.com>
2024-08-24 13:01:00 +0530

    24th August 2024 Code Commited & Pushed

---
+++ src/August/ex_24082024/lab241.java
@@ -0,0 +1,21 @@
+package August.ex_24082024;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+public class lab241 {
+    public static void main(String[] args) {
+        // List - > Duplicate Allows
+        // Set -> Doesn't allow Duplicate
+        Set hd = new HashSet();
+        Set lhs = new LinkedHashSet();
+        Set ts = new TreeSet();
+
+
+
+
+
+
+
+    }
+}
---
+++ src/August/ex_24082024/lab246.java
@@ -0,0 +1,36 @@
+package August.ex_24082024;
+import java.util.*;
+public class lab246 {
+    public static void main(String[] args) {
+        Hashtable<Integer,String> ht1 = new Hashtable<>();
+        // Map
+        // null
+        // Synchronised
+        // Slow
+        // Legacy Class
+
+        ht1.put(1, "one");
+        ht1.put(2, "two");
+        ht1.put(3, "three");
+
+
+        //
+        Enumeration<Integer> e = ht1.keys();
+        while (e.hasMoreElements()){
+            System.out.println(ht1.get(e.nextElement()));
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
